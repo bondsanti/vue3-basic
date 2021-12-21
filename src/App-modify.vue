f6b9b8c4b7cca3fe2c1ed7f67cd3bca6c4697120
santi dev <santi.chpy@gmail.com>
2021-12-22 01:07:23 +0700

    Phase2Finish

--- src/App-modify.vue
+++ src/App-modify.vue
@@ -1,32 +1,63 @@
 <template>
-  <div>
-
-    <FormComponents/>
-      <ListData :employees="employees" />
-  </div>
+  <header><h1>ระบบจัดการข้อมูลพนักงาน</h1></header>
+  <form-components @save="insert" />
+  <section class="emp-content" v-if="employees.length>0">
+    <h2>ช้อมูลพนักงาน</h2>
+    <ListData :employees="employees"/>
+  </section>
 </template>
 
 <script>
-import ListData from './components/ListData.vue'
-import FormComponents from './components/FormComponents.vue'
+import FormComponents from './components/FormComponents.vue';
+import ListData from "./components/ListData.vue";
+
 export default {
   name: "App",
   components: {
-      ListData,
-      FormComponents
+    ListData,
+    FormComponents,
   },
   data(){
-      return{
-          employees:[
-              {name:"ก้อง",salary:15000},
-              {name:"แก้ง",salary:20000},
-              {name:"โก้ง",salary:30000},
-              {name:"เก่ง",},
-          ]
-      }
+    return {
+      employees:[]
+    }
+  },
+  methods:{
+    insert(data){
+      //console.log(d);
+      this.employees.push(data);
+    }
   }
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: rgb(53, 165, 77);
+  color: aliceblue;
+  text-align: center;
+}
+.emp-content {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+}
+.emp-content h2{
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: green;
+  margin: 0 0 1rem 0;
+}
 </style>
---
+++ src/components/Cardtemplate.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+      <img src="https://cdn-icons-png.flaticon.com/128/236/236831.png">
+      <slot name="card-header"></slot>
+      <slot name="card-button"></slot>
+      <slot name="card-content"></slot>
+  </li>
+</template>
+
+<script>
+export default {
+ name:"Cardtemplate"
+}
+</script>
+
+<style scoped>
+li{
+  margin: 1rem 0;
+  font-size: 1.25rem;
+  font-weight: bold;
+  background: rgb(250, 157, 253);
+  padding: 0.25rem;
+  border-radius:15px ;
+  }
+  img{
+      width: 100px;
+      height: 100px;
+  }
+</style>
